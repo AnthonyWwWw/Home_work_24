f605486b86d18544dbf0febe2bf93e9e5cbe8f01
Anton Tsyhanok <anton160400@gmail.com>
2024-06-15 13:15:32 +0300

    end tas

---
+++ React_Task_Swapi/src/components/App.jsx
@@ -0,0 +1,11 @@
+import { Form } from "./FormElement.jsx";
+
+export function App() {
+    return (
+        <section>
+            <div className="container-content custom-width custom-color h-100 mx-auto bg-light">
+                <Form />
+            </div>
+        </section>
+    );
+}
